05a6e19ae69b6af24ac483ce68126bde9c5267b4
tessereth <teresa.bradbury@digital.gov.au>
2017-01-19 14:47:15 +1100

    Add junit test report for circle

--- main_suite_test.go
+++ main_suite_test.go
@@ -1,13 +1,25 @@
 package main_test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 )
 
 func TestMain(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Main Suite")
+	path := "junit.xml"
+	circleReports := os.Getenv("CIRCLE_TEST_REPORTS")
+	if len(circleReports) > 0 {
+		os.Mkdir(fmt.Sprintf("%s/go", circleReports), 0755)
+		path = fmt.Sprintf("%s/go/junit.xml", circleReports)
+		junitReporter := reporters.NewJUnitReporter(path)
+		RunSpecsWithDefaultAndCustomReporters(t, "Main Suite", []Reporter{junitReporter})
+	} else {
+		RunSpecs(t, "Main Suite")
+	}
 }
