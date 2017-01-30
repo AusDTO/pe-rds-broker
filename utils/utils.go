1c9168b6e9b82ea76f144571130fd9a6aa543b0e
tessereth <teresa.bradbury@digital.gov.au>
2017-01-30 13:45:09 +1100

    Add encrypt and decrypt functions

--- utils/utils.go
+++ utils/utils.go
@@ -1,12 +1,18 @@
 package utils
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/base64"
 	"io"
 	"math"
 )
+
+// Must be a multiple of 4
+const PasswordLength = 24
+const UsernameLength = 24
 
 var alpha = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 var numer = []byte("0123456789")
@@ -42,4 +48,57 @@ func GetMD5B64(text string, length int) string {
 	hasher := md5.New()
 	md5 := hasher.Sum([]byte(text))
 	return base64.StdEncoding.EncodeToString(md5)[0:int(math.Min(float64(length), float64(len(md5))))]
+}
+
+func Encrypt(msg string, key, iv []byte) ([]byte, error) {
+	src := []byte(msg)
+	var dst []byte
+
+	aesBlockEncrypter, err := aes.NewCipher(key)
+	if err != nil {
+		return dst, err
+	}
+
+	dst = make([]byte, len(src))
+	aesEncrypter := cipher.NewCFBEncrypter(aesBlockEncrypter, iv)
+	aesEncrypter.XORKeyStream(dst, src)
+
+	return dst, nil
+}
+
+func Decrypt(src, key, iv []byte) (string, error) {
+	dst := make([]byte, len(src))
+
+	aesBlockDecrypter, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	aesDecrypter := cipher.NewCFBDecrypter(aesBlockDecrypter, iv)
+	aesDecrypter.XORKeyStream(dst, src)
+
+	return string(dst), nil
+}
+
+func RandIV() ([]byte, error) {
+	var bytes = make([]byte, aes.BlockSize)
+	_, err := rand.Read(bytes)
+	return bytes, err
+}
+
+func RandString(length int) (string, error) {
+	var bytes = make([]byte, length*3/4)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(bytes), nil
+}
+
+func RandPassword() (string, error) {
+	return RandString(PasswordLength)
+}
+
+func RandUsername() (string, error) {
+	return RandString(UsernameLength)
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -20,3 +20,45 @@ var _ = Describe("GetMD5B64", func() {
 		Expect(md5b64).To(Equal("Y2U3MWI0ODQtZDU0Mi00MGY3LTlkZDQt"))
 	})
 })
+
+var _ = Describe("RandIV", func() {
+	It("returns a random result", func() {
+		one, err := RandIV()
+		Expect(err).NotTo(HaveOccurred())
+		two, err := RandIV()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(one).NotTo(Equal(two))
+	})
+})
+
+var _ = Describe("RandUsername", func() {
+	It("returns a random result", func() {
+		one, err := RandUsername()
+		Expect(err).NotTo(HaveOccurred())
+		two, err := RandUsername()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(one).NotTo(Equal(two))
+	})
+
+	It("returns the correct length", func() {
+		username, err := RandUsername()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(username)).To(Equal(UsernameLength))
+	})
+})
+
+var _ = Describe("RandPassword", func() {
+	It("returns a random result", func() {
+		one, err := RandPassword()
+		Expect(err).NotTo(HaveOccurred())
+		two, err := RandPassword()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(one).NotTo(Equal(two))
+	})
+
+	It("returns the correct length", func() {
+		password, err := RandPassword()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(password)).To(Equal(PasswordLength))
+	})
+})
