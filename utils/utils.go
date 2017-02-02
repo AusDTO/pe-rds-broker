5af61b6d0d3b442bf40d5dc52d74f19ebf34f779
tessereth <teresa.bradbury@digital.gov.au>
2017-02-02 12:35:40 +1100

    Remove unused functions

--- utils/utils.go
+++ utils/utils.go
@@ -3,52 +3,13 @@ package utils
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/md5"
 	"crypto/rand"
 	"encoding/base64"
-	"io"
-	"math"
 )
 
 // Must be a multiple of 4
 const PasswordLength = 24
 const UsernameLength = 24
-
-var alpha = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
-var numer = []byte("0123456789")
-
-func RandomAlphaNum(length int) string {
-	return randChar(1, alpha) + randChar(length-1, append(alpha, numer...))
-}
-
-func randChar(length int, chars []byte) string {
-	newPword := make([]byte, length)
-	randomData := make([]byte, length+(length/4))
-	clen := byte(len(chars))
-	maxrb := byte(256 - (256 % len(chars)))
-	i := 0
-	for {
-		if _, err := io.ReadFull(rand.Reader, randomData); err != nil {
-			panic(err)
-		}
-		for _, c := range randomData {
-			if c >= maxrb {
-				continue
-			}
-			newPword[i] = chars[c%clen]
-			i++
-			if i == length {
-				return string(newPword)
-			}
-		}
-	}
-}
-
-func GetMD5B64(text string, length int) string {
-	hasher := md5.New()
-	md5 := hasher.Sum([]byte(text))
-	return base64.StdEncoding.EncodeToString(md5)[0:int(math.Min(float64(length), float64(len(md5))))]
-}
 
 func Encrypt(msg string, key, iv []byte) ([]byte, error) {
 	src := []byte(msg)
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -7,20 +7,6 @@ import (
 	. "github.com/AusDTO/pe-rds-broker/utils"
 )
 
-var _ = Describe("RandomAlphaNum", func() {
-	It("generates a random alpha numeric with the proper length", func() {
-		randomString := RandomAlphaNum(32)
-		Expect(len(randomString)).To(Equal(32))
-	})
-})
-
-var _ = Describe("GetMD5B64", func() {
-	It("returns the Base64 of a string MD5", func() {
-		md5b64 := GetMD5B64("ce71b484-d542-40f7-9dd4-5526e38c81ba", 32)
-		Expect(md5b64).To(Equal("Y2U3MWI0ODQtZDU0Mi00MGY3LTlkZDQt"))
-	})
-})
-
 var _ = Describe("RandIV", func() {
 	It("returns a random result", func() {
 		one, err := RandIV()
