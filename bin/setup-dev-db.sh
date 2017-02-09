578861ab4b482e7b6c44ad0f4097d7dbbbfb98d7
tessereth <teresa.bradbury@digital.gov.au>
2017-02-09 15:56:34 +1100

    Add script to setup local postgres internaldb
    closes #14

---
+++ bin/setup-dev-db.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+OP=$1
+if [[ -z $OP ]]; then
+    cat <<EOF
+Usage: $0 <create|drop>
+
+Note: This script assumes the current user can create databases.
+If not, you may need to run something like
+
+    sudo -u postgres createuser --createdb $(whoami)
+
+EOF
+    exit 1
+fi
+
+DBNAME=rdsbroker_internaldb
+
+if [[ $OP == "create" ]]; then
+    createdb $DBNAME
+    cat > db.env <<EOF
+export RDSBROKER_INTERNAL_DB_PROVIDER=postgres
+export RDSBROKER_INTERNAL_DB_NAME=$DBNAME
+export RDSBROKER_INTERNAL_DB_URL=localhost
+export RDSBROKER_INTERNAL_DB_PORT=5432
+export RDSBROKER_INTERNAL_DB_USERNAME=$(whoami)
+export RDSBROKER_INTERNAL_DB_PASSWORD=
+export RDSBROKER_INTERNAL_DB_SSLMODE=disable
+EOF
+    cat <<EOF
+Database $DBNAME created
+Environment variables written to db.env
+
+Note: By default, the broker will connect to your database using TCP. If you
+wish to use unix sockets, change RDSBROKER_INTERNAL_DB_URL to the correct
+socket directory for your system (probably either /tmp or /var/run/postgresql).
+
+EOF
+elif [[ $OP == "drop" ]]; then
+    dropdb $DBNAME
+    echo "Database $DBNAME dropped"
+fi
