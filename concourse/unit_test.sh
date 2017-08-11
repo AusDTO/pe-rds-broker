2e7469f4b4e5cdb62cf8c816a8fb2d6099cc7aec
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-08-11 14:04:20 +1000

    test

--- concourse/unit_test.sh
+++ concourse/unit_test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "yo!"
+set -xe
+
+export GOPATH=$PWD/go
+export PATH=$GOPATH/bin:$PATH
+
+go get github.com/onsi/ginkgo/ginkgo github.com/Masterminds/glide
+glide install
+go build -v -i
+ginkgo -r -race
