5e8de1c980c2571457683c164c849150ff4f6a7b
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-08-11 14:05:42 +1000

    Fixup

--- concourse/unit_test.sh
+++ concourse/unit_test.sh
@@ -6,6 +6,8 @@ export GOPATH=$PWD/go
 export PATH=$GOPATH/bin:$PATH
 
 go get github.com/onsi/ginkgo/ginkgo github.com/Masterminds/glide
+
+cd $GOPATH/src/github.com/AusDTO/pe-rds-broker
 glide install
 go build -v -i
 ginkgo -r -race
