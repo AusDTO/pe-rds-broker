28c9505ede62f6b87bd3e7d914b22d2eb7eccda9
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-11-21 11:56:07 +1100

    Deal with structs not being struct pointers

--- config.go
+++ config.go
@@ -27,24 +27,27 @@ func LoadConfig(envVars *cfcommon.EnvVars, configFile string) (config *Config, e
 
 	bytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		return config, err
+		return nil, err
 	}
 
 	if err = yaml.Unmarshal(bytes, &config); err != nil {
-		return config, err
+		return nil, err
 	}
 
 	config.RDSConfig.DBPrefix = envVars.MustString("DB_PREFIX")
 	for _, service := range config.RDSConfig.Catalog.Services {
-		for _, sp := range service.Plans {
+		for idx, sp := range service.Plans {
 			sp.RDSProperties.MultiAZ = envVars.Bool("DB_MULTI_AZ")
 			sp.RDSProperties.DBSubnetGroupName = envVars.MustString("DB_RDS_SUBNET_GROUP_NAME")
 			sp.RDSProperties.DBSecurityGroups = []string{envVars.MustString(fmt.Sprintf("DB_RDS_SECURITY_GROUP_%s", strings.ToUpper(sp.RDSProperties.Engine)))}
+
+			// Note, since this is a slice of structs, not struct pointers, we need to explicit set it after making changes
+			service.Plans[idx] = sp
 		}
 	}
 
 	if err = config.Validate(); err != nil {
-		return config, fmt.Errorf("Validating config contents: %s", err)
+		return nil, fmt.Errorf("Validating config contents: %s", err)
 	}
 
 	return config, nil
